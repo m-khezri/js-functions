a5981264277754645117f798ab525510ad2f68c5
mohammad <khezri@outlook.com>
2018-08-25 10:17:11 -0500

    making files

---
+++ main.js
@@ -0,0 +1,16 @@
+const bananaDiv = document.getElementById("bananas");
+bananaDiv.innerHTML = 'Im a banana';
+
+let counter = 0;
+counter == 1;
+counter++;
+
+// counter = 3
+console.log('counter: ', counter);
+
+const firstname = 'Mohammad';
+const lastName = 'Khezri';
+
+const greetingElement = document.getElementById("greeting");
+
+greetingElement.innerHTML = `Hello my name is ${firstname} ${lastName}`;
