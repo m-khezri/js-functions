e6add613e551709e33c06f5a65d566f62cc82f37
mohammad <khezri@outlook.com>
2018-08-25 10:26:49 -0500

    making js

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ const lastName = 'Khezri';
 const greetingElement = document.getElementById("greeting");
 
 greetingElement.innerHTML = `Hello my name is ${firstname} ${lastName}`;
+
+// object shorthand notation
+
+// let name = 'callan';
+// let age = '100000000000';
+
+let person = {name, age};
+
+console.log('person: ' , person.name);
